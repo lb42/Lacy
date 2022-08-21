658b711ca9bc622edd553905cd262fb2f089941d
lb42 <lou.burnard@retired.ox.ac.uk>
2022-08-21 17:58:28 +0100

    checking numbers

---
+++ vols.sh
@@ -0,0 +1,88 @@
+# https://hdl.handle.net/2027/mdp.39015005887966 (vol 90)
+9-85 L1335 # new men
+86-153 L1336 #rienzi
+154-185 L1337 #innkeeper
+186-218 L1338 #whitecat
+219-273 L1339 #oneoclock
+274-289 L1340 #xmaseve
+290-308 L1341 #romantic
+309-367 L1342 #behind
+368-403 L1343 #lady
+404-460 L1344 #white
+463-501 L1345 #plot
+502-538 L1346 #dora [lots of damage from here on]
+539-577 L1347 #perfect
+578-611 L1348 #worth
+612-631 L1349 #misstibbet
+
+#https://hdl.handle.net/2027/mdp.39015067453749 (vol 79)
+6-37 L1171 #ifihad
+38-71 L1172 #caesar
+73-109 L1173 #inchcape
+110-133 L1174 #siamese
+134-171 L1175 #humbug
+172-219 L1176 #white
+220-235 L1177 #upfor
+236-270 L1178 #caliph
+271-297 L1179 #mischief
+298-317 L1180 #ahome
+318-373 L???? #aglass
+375-418 L1181 #timour
+420-445 L1182 #irishman
+446-491 L1183 #george
+492-517 L1184 #whostowin
+
+#https://hdl.handle.net/2027/mdp.39015067453731 (vol 85)
+10-87 L1260 #manof
+88-115 L1261 #pedrillo
+116-157 L1262 #oldscore
+158-201 L1263 #milkywhite
+202-243 L1264 #checkmate
+244-293 L1265 #felons
+295-329 L1266 #broken
+330-375 L1267 #hop-pickers
+376-419 L1268 #loving
+420-465 L1269 #homewreck
+467-496 L1270 #spoiled
+498-545 L1271 #daddy
+546-605 L1272 #serpent
+606-621 L1273 #seagulls
+622-645 L1274 #oldgooseberry
+
+#https://hdl.handle.net/2027/mdp.39015019190373 (vol 82)
+
+10-71 L1215 #wifes
+72-84 L1216 #beautiful
+86-106 L1217 #atchi
+108-133 L1218 #stranger
+134-217 L1219 #settling
+219-257 L1220 #presumptive
+259-281 L1221 #chronono
+283-311 L1222 #oldand
+313-357 L1223 #graceh
+359-399 L1224 #wizard
+400-457 L1225 #dead
+458-494 L1226 #brown #damaged
+# 495 advert
+496-521 L1227 #irish
+522-551 L1228 #philippe
+553-587 L1229 #comfortable
+
+
+#https://hdl.handle.net/2027/mdp.39015067453830 (vol 51)
+10-63 L0751 #blacksheep
+64-107 L0752 #pirates
+108-145 L0753 #maccarthy
+146-163 L0754 #turkish
+164-185 L0755 #pacha
+186-236 L0756 #scrap
+# 237 LAE
+238-281 L0757 #jocrisse
+282-313 L0758 #oldstory
+314-369 L0759 #speed
+370-397 L0760 #telemachus
+398-453 L0761 #angel
+454-473 L0762 #onandoff
+474-497 L0763 #moreprecious
+498-515 L0764 #peace
+516-531 L0765 #pretty [pp 1-4  missing
