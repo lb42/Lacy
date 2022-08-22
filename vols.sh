a273bbffd2e799a5cac8d571f3cb8083af230f6e
lb42 <lou.burnard@retired.ox.ac.uk>
2022-08-22 15:31:24 +0100

    more HT titles

--- vols.sh
+++ vols.sh
@@ -1,73 +1,61 @@
-# https://hdl.handle.net/2027/mdp.39015005887966 (vol 90)
-9-85 L1335 # new men
-86-153 L1336 #rienzi
-154-185 L1337 #innkeeper
-186-218 L1338 #whitecat
-219-273 L1339 #oneoclock
-274-289 L1340 #xmaseve
-290-308 L1341 #romantic
-309-367 L1342 #behind
-368-403 L1343 #lady
-404-460 L1344 #white
-463-501 L1345 #plot
-502-538 L1346 #dora [lots of damage from here on]
-539-577 L1347 #perfect
-578-611 L1348 #worth
-612-631 L1349 #misstibbet
+#https://hdl.handle.net/2027/mdp.39015067453822 vol 06
+10-26 L0076 #duel
+28-44 L0077 #kiss
+#45 #advert
+46-73 L0078 #cherry
+74-117 L0079 #corsican
+118-144 L0080 #our
+#145 #advert
+146-166 L0081 #matrimonial
+168-192 L0082 #elhyder
+194-228 L0083 #twelve
+230-248 L0084 #whostole
+250-277 L0085 #wittikind
+278-295 L0086 #spanking
+296-355 L0087 #kingof
+356-384 L0088 #ganem
+#385 #advert
+386-405 L0089 #taking
+406-427 L0090 #verysuspicious
 
-#https://hdl.handle.net/2027/mdp.39015067453749 (vol 79)
-6-37 L1171 #ifihad
-38-71 L1172 #caesar
-73-109 L1173 #inchcape
-110-133 L1174 #siamese
-134-171 L1175 #humbug
-172-219 L1176 #white
-220-235 L1177 #upfor
-236-270 L1178 #caliph
-271-297 L1179 #mischief
-298-317 L1180 #ahome
-318-373 L???? #aglass
-375-418 L1181 #timour
-420-445 L1182 #irishman
-446-491 L1183 #george
-492-517 L1184 #whostowin
 
-#https://hdl.handle.net/2027/mdp.39015067453731 (vol 85)
-10-87 L1260 #manof
-88-115 L1261 #pedrillo
-116-157 L1262 #oldscore
-158-201 L1263 #milkywhite
-202-243 L1264 #checkmate
-244-293 L1265 #felons
-295-329 L1266 #broken
-330-375 L1267 #hop-pickers
-376-419 L1268 #loving
-420-465 L1269 #homewreck
-467-496 L1270 #spoiled
-498-545 L1271 #daddy
-546-605 L1272 #serpent
-606-621 L1273 #seagulls
-622-645 L1274 #oldgooseberry
+#https://hdl.handle.net/2027/mdp.39015053749829 (vol 22)
+10-33 L0316 #morning
+34-57 L0317 #haymarket
+58-77 L0318 #toomuch
+78-135 L0319 #still
+137-227 L0320 #henry
+228-253 L0321 #garrick
+254-277 L0322 #buckstones
+278-317 L0323 #dowager
+319-348 L0324 #young
+350-401 L0325 #helping
+402-459 L0326 #stranger
+460-482 L0327 #howstout
+484-550 L0328 #shewould
+552-570 L0329 #only
+#ad #571
+572-607 L0330 #mountain
 
-#https://hdl.handle.net/2027/mdp.39015019190373 (vol 82)
+#https://hdl.handle.net/2027/mdp.39015067453277 (vol 40)
+10-63 L0586 #everybodys
+65-101 L0587 #richard
+103-127 L0588 #hunting
+128-157 L0589 #which
+159-185 L0590 #king
+186-233 L0591 #dream
+234-255 L0592 #ici
+257-288 L0593 #turning
+#289 #advert
+291-321 L0594 #seven
+322-345 L0595 #ivewritten
+346-417 L0596 #julius
+419-449 L0597 #three
+451-499 L0598 #whitefriars
+500-511 L0599 #rifle
+512-547 L0600 #nine
 
-10-71 L1215 #wifes
-72-84 L1216 #beautiful
-86-106 L1217 #atchi
-108-133 L1218 #stranger
-134-217 L1219 #settling
-219-257 L1220 #presumptive
-259-281 L1221 #chronono
-283-311 L1222 #oldand
-313-357 L1223 #graceh
-359-399 L1224 #wizard
-400-457 L1225 #dead
-458-494 L1226 #brown #damaged
-# 495 advert
-496-521 L1227 #irish
-522-551 L1228 #philippe
-553-587 L1229 #comfortable
-
+#https://hdl.handle.net/2027/mdp.39015067453277 (vol 50
 
 #https://hdl.handle.net/2027/mdp.39015067453830 (vol 51)
 10-63 L0751 #blacksheep
@@ -86,3 +74,153 @@
 474-497 L0763 #moreprecious
 498-515 L0764 #peace
 516-531 L0765 #pretty [pp 1-4  missing
+
+#https://hdl.handle.net/2027/mdp.39015067453822 (vol 54)
+10-30 L0796 #villikins
+#31 #laelist
+32-50 L0797 #eclipsing
+#51 #laelist (to v15)
+52-75 L0798 #margery
+76-111 L0799 #oldphil
+112-129 L0800 #mother
+130-143 L0801 #fairys
+144-169 L0802 #orange
+170-191 L0803 #intrigue
+192-243 L0804 #lifes
+244-311 L0805 #friends
+312-341 L0806 #wifes
+342-355 L0807 #caught
+356-375 L0808 #nice
+376-403 L0809 #catch
+404-457 L0810 #idiot
+
+
+#https://hdl.handle.net/2027/mdp.39015067453780 (vol64)
+10-57 L0946 #how
+59-130 L0947 #henry
+#131 #ad
+132-149 L0948 #doing
+150-177 L0949 #mywife
+179-265 L0950 #cymbeline
+266-309 L0951 #snowdrop
+310-372 L0952 #sybilla
+#373 #ad
+374-399 L0953 #lad
+400-453 L0954 #mothers
+454-489 L0955 #grinbushes
+490-525 L0956 #cupid
+526-568 L0957 #hercules
+#569 #ad
+570-587 L0958 #knottingem
+588-617 L0959 #princess
+618-661 L0960 #lesson
+
+#https://hdl.handle.net/2027/mdp.39015067453764 (vol74
+10-61 L1096 #yew
+62-83 L1097 #suit
+85-135 L1098 #wife
+137-163 L1099 #daughter
+164-227 L1100 #theodora
+228-246 L1101 #endof
+249-289 L1102 #monsieur
+290-310 L1103 #fighting
+313-353 L1104 #simpson
+354-375 L1105 #atrocious
+376-409 L1106 #handy
+411-485 L1107 #love
+487-518 L1108 #mineralo
+520-542 L1109 #davenport
+544-593 L1110 #theliar
+
+#https://hdl.handle.net/2027/mdp.39015067453749 (vol 79)
+6-37 L1171 #ifihad
+38-71 L1172 #caesar
+73-109 L1173 #inchcape
+110-133 L1174 #siamese
+134-171 L1175 #humbug
+172-219 L1176 #white
+220-235 L1177 #upfor
+236-270 L1178 #caliph
+271-297 L1179 #mischief
+298-317 L1180 #ahome
+318-373 L???? #aglass
+375-418 L1181 #timour
+420-445 L1182 #irishman
+446-491 L1183 #george
+492-517 L1184 #whostowin
+
+
+#https://hdl.handle.net/2027/mdp.39015067453590 (vol 80: many pp unusable)
+10-55 L1185 #maud
+56-69 L1186 #verypleasant
+70-114 L1187 #peepshow
+116-141 L1188 #onetoomany
+143-176 L1189 #happiest
+178-201 L1190 #pleasant
+203-242 L1191 #xyz
+244-269 L1192 #volunteer
+271-297 L1193 #whos
+298-355 L1194 #lost
+356-409 L1195 #honour
+410-433 L1196 #silent
+434-477 L1197 #field
+478-505 L1198 #special
+506-521 L1199 #goto
+
+
+#https://hdl.handle.net/2027/mdp.39015019190373 (vol 82)
+10-71 L1215 #wifes
+72-84 L1216 #beautiful
+86-106 L1217 #atchi
+108-133 L1218 #stranger
+134-217 L1219 #settling
+219-257 L1220 #presumptive
+259-281 L1221 #chronono
+283-311 L1222 #oldand
+313-357 L1223 #graceh
+359-399 L1224 #wizard
+400-457 L1225 #dead
+458-494 L1226 #brown #damaged
+# 495 advert
+496-521 L1227 #irish
+522-551 L1228 #philippe
+553-587 L1229 #comfortable
+
+#84
+
+#https://hdl.handle.net/2027/mdp.39015067453731 (vol 85)
+10-87 L1260 #manof
+88-115 L1261 #pedrillo
+116-157 L1262 #oldscore
+158-201 L1263 #milkywhite
+202-243 L1264 #checkmate
+244-293 L1265 #felons
+295-329 L1266 #broken
+330-375 L1267 #hop-pickers
+376-419 L1268 #loving
+420-465 L1269 #homewreck
+467-496 L1270 #spoiled
+498-545 L1271 #daddy
+546-605 L1272 #serpent
+606-621 L1273 #seagulls
+622-645 L1274 #oldgooseberry
+
+
+# https://hdl.handle.net/2027/mdp.39015005887966 (vol 90)
+9-85 L1335 # new men
+86-153 L1336 #rienzi
+154-185 L1337 #innkeeper
+186-218 L1338 #whitecat
+219-273 L1339 #oneoclock
+274-289 L1340 #xmaseve
+290-308 L1341 #romantic
+309-367 L1342 #behind
+368-403 L1343 #lady
+404-460 L1344 #white
+463-501 L1345 #plot
+502-538 L1346 #dora [lots of damage from here on]
+539-577 L1347 #perfect
+578-611 L1348 #worth
+612-631 L1349 #misstibbet
+
+#92
